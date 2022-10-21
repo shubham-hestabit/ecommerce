05e50a0492a6db9464e88a04d00620c775ca507a
Shubham Saini <shubhamsaini.hestabit@gmail.com>
2022-10-21 11:37:58 +0530

    update user logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,15 +56,15 @@
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item dropdown user-menu">
                     <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
-                        <img src="https://assets.infyom.com/logo/blue_logo_150x150.png"
+                        <img src="https://img.icons8.com/plasticine/2x/user.png"
                             class="user-image img-circle elevation-2" alt="User Image">
                         <span class="d-none d-md-inline">{{ Auth::user()->name }}</span>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
                         <!-- User image -->
                         <li class="user-header bg-primary">
-                            <img src="https://assets.infyom.com/logo/blue_logo_150x150.png"
-                                class="img-circle elevation-2" alt="User Image">
+                            <img src="https://img.icons8.com/plasticine/2x/user.png"
+                                class="user-image img-circle elevation-2" alt="User Image">
                             <p>
                                 {{ Auth::user()->name }}
                                 @php
