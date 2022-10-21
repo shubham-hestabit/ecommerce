adf0155b77fe61195410ca8cde100832f121e4da
Shubham Saini <shubhamsaini.hestabit@gmail.com>
2022-10-21 11:24:29 +0530

    update logo in sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,8 +1,12 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="{{ route('home') }}" class="brand-link">
-        <img src="https://assets.infyom.com/logo/blue_logo_150x150.png" alt="AdminLTE Logo"
+        <!-- <img src="https://assets.infyom.com/logo/blue_logo_150x150.png" alt="AdminLTE Logo"
             class="brand-image img-circle elevation-3">
-        <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
+        <span class="brand-text font-weight-light">{{ config('app.name') }}</span> -->
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzDHPihbUCvrHPS-HeWgxhYcVXK4jV9MiZ1Q&usqp=CAU"
+            alt="AdminLTE Logo" class="brand-image img-circle elevation-3">
+
+        <span class="brand-text font-weight-light">E-Commerce</span>
     </a>
 
     <div class="sidebar">
