37046f04d91c5c95b30ffe7284ab4bdca7e9f44a
Shubham Saini <shubhamsaini.hestabit@gmail.com>
2022-10-21 13:40:29 +0530

    fix an issue in api.php file

--- routes/api.php
+++ routes/api.php
@@ -26,19 +26,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
  */
 
 // Route for viewing all categories.
-Route::get('/category-index', [CategoryController::class, 'indexa']);
+Route::get('/category-index', [CategoryController::class, 'index']);
 
 // Route for insert categories.
-Route::post('/category-insert', [CategoryController::class, 'insert']);
+Route::post('/category-insert', [CategoryController::class, 'store']);
 
 // Route for view a category.
-Route::get('/category-read/{id}', [CategoryController::class, 'read']);
+Route::get('/category-read/{id}', [CategoryController::class, 'show']);
 
 // Route for update a category.
 Route::put('/category-update/{id}', [CategoryController::class, 'update']);
 
 // Route for delete a category.
-Route::delete('/category-delete/{id}', [CategoryController::class, 'delete']);
+Route::delete('/category-delete/{id}', [CategoryController::class, 'destroy']);
 
 
 
@@ -50,16 +50,16 @@ Route::delete('/category-delete/{id}', [CategoryController::class, 'delete']);
 Route::get('/sub-category-index', [SubCategoryController::class, 'index']);
 
 // Route for insert sub categories.
-Route::post('/sub-category-insert', [SubCategoryController::class, 'insert']);
+Route::post('/sub-category-insert', [SubCategoryController::class, 'store']);
 
 // Route for view a sub category.
-Route::get('/sub-category-read/{id}', [SubCategoryController::class, 'read']);
+Route::get('/sub-category-read/{id}', [SubCategoryController::class, 'show']);
 
 // Route for update a sub category.
 Route::put('/sub-category-update/{id}', [SubCategoryController::class, 'update']);
 
 // Route for delete a sub category.
-Route::delete('/sub-category-delete/{id}', [SubCategoryController::class, 'delete']);
+Route::delete('/sub-category-delete/{id}', [SubCategoryController::class, 'destroy']);
 
 
 
@@ -71,13 +71,13 @@ Route::delete('/sub-category-delete/{id}', [SubCategoryController::class, 'delet
 Route::get('/product-index', [ProductController::class, 'index']);
 
 // Route for insert products.
-Route::post('/product-insert', [ProductController::class, 'insert']);
+Route::post('/product-insert', [ProductController::class, 'store']);
 
 // Route for view a product.
-Route::get('/product-read/{id}', [ProductController::class, 'read']);
+Route::get('/product-read/{id}', [ProductController::class, 'show']);
 
 // Route for update a product details.
 Route::put('/product-update/{id}', [ProductController::class, 'update']);
 
 // Route for delete a product.
-Route::delete('/product-delete/{id}', [ProductController::class, 'delete']);
+Route::delete('/product-delete/{id}', [ProductController::class, 'destroy']);
