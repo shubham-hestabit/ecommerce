3f89907db4f5fa74df240176864119c29ae0e6eb
Shubham Saini <shubhamsaini.hestabit@gmail.com>
2022-11-02 13:02:39 +0530

    update images link

--- resources/views/layouts/ecommerce/payment.blade.php
+++ resources/views/layouts/ecommerce/payment.blade.php
@@ -160,7 +160,8 @@
 </div>
 
 <script src="https://code.jquery.com/jquery-1.12.3.min.js"
-    integrity="sha256-aaODHAgvwQW1bFOGXMeX+pC4PZIPsvn2h1sArYOhgXQ=" crossorigin="anonymous"></script>
+    integrity="sha256-aaODHAgvwQW1bFOGXMeX+pC4PZIPsvn2h1sArYOhgXQ=" crossorigin="anonymous">
+</script>
 
 <script>
 $(function() {
